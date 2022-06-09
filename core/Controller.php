38c6d321d8d784a058f5911f71efda4816d57852
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-09 11:21:07 +0100

    Ajouter des commentaires explicatifs

--- core/Controller.php
+++ core/Controller.php
@@ -1,11 +1,20 @@
 <?php
 namespace app\core;
 class Controller{
+    //Par defaut le layout est main
     public string $layout='main';
-    
+    /**
+     * Permet de modifier le layout principale de la page
+     * @param string $layout  le nom du layout
+     */
     public function setLayout($layout){
         $this->layout = $layout;
     }
+
+    /**
+     * 
+     * @param array $params null si methode get sinon si post on passe les params envoyer par post
+     */
     public function render($views, $params = []) {
         return Application::$app->router->renderView($views, $params);
     }
