39d12d8dc8b74b912f0a83f8354933ff075902d5
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-11 11:48:13 +0100

    implementer la page contact view ajouter le formulaire de contact

--- views/contact.php
+++ views/contact.php
@@ -6,7 +6,7 @@
     <!--Section description-->
     <p class="text-center mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
         a matter of hours to help you.</p>
-
+ 
     <div class="row">
 
         <!--Grid column-->
