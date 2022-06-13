77ff6ec2a4eadc928b17a33e9801ca842c99427f
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-13 11:30:05 +0100

    Afficher la liste de produits

--- views/dashProducts.php
+++ views/dashProducts.php
@@ -6,11 +6,13 @@
   <thead class="thead-dark">
     <tr>
       <th scope="col">id</th>
+      <th scope="col">Image</th>
       <th scope="col">Titre</th>
       <th scope="col">Description</th>
       <th scope="col">Quantité en stock</th>
       <th scope="col">Prix</th>
-      <th scope="col">Disponibilité</th>
+      <th scope="col">Sous categorie</th>
+      <th scope="col">Categorie</th>
       <th scope="col">Opérations</th>
     </tr>
   </thead>
@@ -18,11 +20,15 @@
       <?php foreach($produits as $produit) : ?>
       <tr>
           <td><?php echo $produit['id']; ?></td>
-          <td><?php echo $produit['titre_produit']; ?></td>
-          <td><?php echo $produit['description_produit']; ?></td>
-          <td><?php echo $produit['quantite_produit']; ?></td>
-          <td><?php echo $produit['prix_produit']; ?></td>
-          <td><?php echo $produit['dispo_produit']; ?></td>
+          <td> 
+          <img src="../Assets/images/<?php echo $image['chemin'] ?>" id="" class="" width="100px" />
+            </td>
+          <td><?php echo $produit['titre']; ?></td>
+          <td><?php echo $produit['description']; ?></td>
+          <td><?php echo $produit['quantite']; ?></td>
+          <td><?php echo $produit['prix']; ?></td>
+          <td><?php echo $s_categorie['libelle']; ?></td>
+          <td><?php echo $categorie['libelle']; ?></td>
           <td>
               <a class="link-secondary" href="./updateClass?id=<?php echo $produit['id']; ?>"><i class="bi bi-pencil-square"></i></a>
               <a class="link-secondary" href="./delete?id=<?php echo $produit['id']; ?>"><i class="bi bi-trash"></i></a> 
