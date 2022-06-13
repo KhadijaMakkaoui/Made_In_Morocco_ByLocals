6109776315dcda1dd252336ba2f72b2f9fd2a9fd
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-13 21:14:29 +0100

    implementer un model pour la table sous categories

---
+++ models/SousCategorie.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use app\core\DbModel;
+
+
+class SousCategorie extends DbModel
+{
+    public string $libelle;
+    public string $description;
+    public int $fk_categorie;
+
+    public function tableName(): string
+    {
+        return 'sous_categories';
+    }
+    public function attributes(): array
+    {
+        return [ 
+            'libelle',            
+            'description',
+            'fk_categorie'
+            ];
+    }
+
+    public function update(int $id)
+    {
+        return parent::update($id);
+    }
+
+    public function selectAll($attr=[])
+    {
+        return parent::selectAll();
+    }
+    // public function selectAll($attr=[])
+    // {
+    //     return parent::selectAll();
+    // }
+
+    public function select(int $id)
+    {
+        return parent::select($id);
+    }
+
+    public function delete(int $id)
+    {
+        return parent::delete($id);
+    }
+
+    public function save()
+    {
+        return parent::save();
+    }
+    public function rules(): array
+    {
+        return [
+    //         'matricule' => [self::RULE_REQUIRED, [
+    //             self::RULE_UNIQUE, 'class' => self::class
+    //         ]],            
+    //         'firstname' => [self::RULE_REQUIRED],
+    //         'lastname' => [self::RULE_REQUIRED],
+    //         'gender' => [self::RULE_REQUIRED],
+    //         'matiere' => [self::RULE_REQUIRED],
+    //         'phone' => [self::RULE_REQUIRED,[
+    //             self::RULE_UNIQUE, 'class' => self::class
+    //         ]],  
+        ];
+    }
+
+}
