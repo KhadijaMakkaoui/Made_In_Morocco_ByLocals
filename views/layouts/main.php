d993278f3d8e6bf301c6373a54278e71eaab0748
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-15 22:34:24 +0100

    Ajouter un query string dans les lien de dropdown list des categories contenat l'id de chaque categorie

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,10 +35,10 @@
                             <span class="dropdown-toggle nav-link " id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                 Nos catégories</span>
                             <ul class="dropdown-menu  ">
-                                <li><a href="/productsBycat/maison_decoration" class="nav-link">Maison et Décorations</a></li>
-                                <li><a href="/productsBycat/vetement_accessoirs" class="nav-link">Vêtement et accessoirs</a></li>
-                                <li><a href="/productsBycat/herbes_pices_fruits_secs" class="nav-link">Herbes, épices et fruits secs</a></li>
-                                <li><a href="/productsBycat/sante_cosmetique" class="nav-link">Santé et cosmetique</a></li>
+                                <li><a href="/productsByCat?categorie=1" class="nav-link">Maison et Décorations</a></li>
+                                <li><a href="/productsByCat/categorie=2" class="nav-link">Vêtement et accessoirs</a></li>
+                                <li><a href="/productsByCat/categorie=3" class="nav-link">Herbes, épices et fruits secs</a></li>
+                                <li><a href="/productsByCat/categorie=4" class="nav-link">Santé et cosmetique</a></li>
                             </ul>
                         </li>
                         <li class="nav-item"><a class="nav-link" href="#">Blogue et histoires</a></li>
