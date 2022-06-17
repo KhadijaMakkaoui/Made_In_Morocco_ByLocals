fea2ec3ab5618c66b0d8a9aff5abc34f887f5fa0
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-17 09:15:21 +0100

    Creer la methode categorieOfSCategorie() permet de recuperer la categorie d'une sous categorie donnée

--- models/Categorie.php
+++ models/Categorie.php
@@ -31,10 +31,6 @@ class Categorie extends DbModel
     {
         return parent::selectAll();
     }
-    // public function selectAll($attr=[])
-    // {
-    //     return parent::selectAll();
-    // }
 
     public function select(int $id)
     {
@@ -49,6 +45,16 @@ class Categorie extends DbModel
     public function save()
     {
         return parent::save();
+    }
+
+    public function categorieOfSCategorie($id_s_categorie)
+    {  
+        $tableName = $this->tableName();
+        $statement = self::prepareIt("SELECT c.* FROM $tableName as c INNER JOIN sous_categories ON c.id=sous_categories.fk_categorie
+        WHERE sous_categories.id=$id_s_categorie");
+        $statement->execute();
+        $result= $statement->fetch(\PDO::FETCH_ASSOC);
+        return $result;
     }
     public function rules(): array
     {
