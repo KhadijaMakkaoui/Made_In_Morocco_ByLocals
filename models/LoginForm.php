643dd23c25eb295d83fa6cef717df6eef5ba167a
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-06-21 00:01:33 +0100

    traduire les labels et les messages à afficher en fr

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -19,18 +19,18 @@ class LoginForm extends Model{
     public function login(){
         $user = User::findOne(['email' => $this->email]);
         if(!$user){
-            $this->addError('email','User does not exists with this email');
+            $this->addError('email',"Cet email n'existe pas");
             return false;           
         }
         if(!password_verify($this->password,$user->password)){
-            $this->addError('password','Password is incorrect');
+            $this->addError('password','Mot de passe incorrecte');
             return false;
         }
         return Application::$app->login($user);
     }
     public function labels():array{
         return [
-            'email' => 'Your email',
-            'password' => 'Password'];
+            'email' => 'Adresse email',
+            'password' => 'Mot de passe'];
     }
 }
